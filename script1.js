062b68f7c07b8bab942f44b7e114fa88ec6b995f
FluffyPoro <lukegabrieldimailig@gmail.com>
2024-12-15 23:38:49 +0800

    script1.js

--- script1.js
+++ script1.js
@@ -1,16 +1,12 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get username and password values
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    // Simulate a basic login check (for demonstration purposes)
-    if (username === 'user' && password === 'password') {
-      alert('Login successful!');
-      window.location.href = 'plan.html';  // Redirect to another page on successful login
-    } else {
-      // Display an error message if login fails
-      document.getElementById('error-message').textContent = 'Invalid username or password!';
-    }
-  });
+  event.preventDefault(); 
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+
+  console.log('Username:', username);
+  console.log('Password:', password);
+
+  window.location.href = 'plan.html';
+});
